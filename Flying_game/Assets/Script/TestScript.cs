877e1f5daaf9e364e82b33806886465488d16576
Mercury <qmercury@naver.com>
2022-05-15 17:26:10 +0900

    프로젝트 테스트코드 출력완료
    정상출력 확인

---
+++ Flying_game/Assets/Script/TestScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        Debug.Log("Hello Unity World");
+    }     
+}
